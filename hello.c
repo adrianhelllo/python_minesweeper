28b03bb95e5507b3586032efa17b16c752131375
adrianhelllo <hetessyadrian@gmail.com>
2025-08-14 18:08:07 +0200

    ooo coolio printing

--- hello.c
+++ hello.c
@@ -2,24 +2,25 @@
 #include <string.h>
 #include <windows.h>
 
-char MESSAGE[] = "End suicide and drink dr. peeper";
+char MESSAGE[] = "Hello World!";
 
 int main(void)
 {
     int len = strlen(MESSAGE);
 
-    char output_msg[len];
+    char output_msg[len + 1];
+    output_msg[len] = '\0';
 
     for (int i = 0; i <= len; i++)
     {
         char msg_char = MESSAGE[i];
 
-        for (int j = 0, targ = msg_char; i <= targ; i++)
+        for (int j = 0, targ = msg_char; j <= targ; j++)
         {
-            output_msg[]
-            printf("%c", (char) i);
-            Sleep(50);
+            output_msg[i] = (char) j;
+            output_msg[i + 1] = '\0';
+            printf("%s\n", output_msg);
+            Sleep(1);
         }
     }
-    printf("\n");
 }
